bce4a8b4542f3676257b20500591254f5e845292
MinhoJJang <ahchjang@naver.com>
2022-11-18 19:16:19 +0900

    1118

--- Problem solving techniques/teamProject/main.c
+++ Problem solving techniques/teamProject/main.c
@@ -1,44 +1,73 @@
 #include "linkedList.h"
 
-typedef struct _NODE
-{
-    int tag;
-    char registeredDate[LEN];
-    char feePaid[LEN];
-    char name[LEN];
-    int age;
-    char organization[LEN];
-    char job[LEN];
-} PersonInfo;
+// typedef struct _NODE
+// {
+//     int tag;
+//     int year;
+//     int month;
+//     int day;
+//     char answer[LEN];
+//     char name[LEN];
+//     int age;
+//     char organization[LEN];
+//     char job[LEN];
+// } PersonInfo;
 
 // 노드 타입 정의
-typedef struct _node
-{
-    PersonInfo data;     // 노드 데이터
-    struct _node *pNext; // 다음 노드 포인터
-} Node;
+// typedef struct _node
+// {
+//     PersonInfo data;     // 노드 데이터
+//     struct _node *pNext; // 다음 노드 포인터
+// } Node;
 
-typedef struct _linkedList
-{
-    Node *pHead;
-    Node *pTail;
-    Node *pCurrent;
-    int numData;
-} LinkedList;
+// typedef struct _linkedList
+// {
+//     Node *pHead;
+//     Node *pTail;
+//     Node *pCurrent;
+//     int numData;
+// } LinkedList;
 
-typedef LinkedList List;
+// typedef LinkedList List;
 
 // 구조체 타입 배열과 리스트를 각각 생성한다.
-PersonInfo info_arr[LEN];
+
 List *info_list;
+
+void printArr(PersonInfo info[], int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d/%d-%d-%d/%s/%s/%d/%s/%s\n", info[i].tag, info[i].year, info[i].month, info[i].day, info[i].answer, info[i].name, info[i].age, info[i].organization, info[i].job);
+    }
+}
 
 int main()
 {
-    // 1. 먼저, 배열에 주어진 데이터를 삽입
+    PersonInfo info[LEN];
+    FILE *inFile;
+    inFile = fopen("./registration_data.txt", "r");
+    int len = 0;
+    /*
+    6/2020-06-04/yes/Bobby Anderson/33/McGill University/engineer
+    */
 
-    // 2. 리스트에다 주어진 데이터를 삽입
+    // 202033762 장민호
+    // info 배열에 데이터 삽입하기
+    if (inFile != NULL)
+    {
+        int i = 0;
+        while (!feof(inFile))
+        {
+            fscanf(inFile, "%d/%d-%d-%d/%[^/]/%[^/]/%d/%[^/]/%s", &info[i].tag, &info[i].year, &info[i].month, &info[i].day, info[i].answer, info[i].name, &info[i].age, info[i].organization, info[i].job);
+            i++;
+        }
+        len = i - 1;
 
-    // -> 과제에서 원하는 함수 사용하여 진행
+        fclose(inFile);
+    }
+
+    printArr(info, len);
 
     return 0;
 }
